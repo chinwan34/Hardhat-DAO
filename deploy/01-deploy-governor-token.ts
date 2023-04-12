e57dc66202ecb289d834cf44b147204a190bf1b8
Chin Wan <k21016106@kcl.ac.uk>
2023-04-12 22:05:39 +0200

    Created deploy script for governance token and timelock

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -1,11 +1,32 @@
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {DeployFunction} from "hardhat-deploy/types";
+import {ethers} from "hardhat";
 
 const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const {getNamedAccounts, deployments, network} = hre;
+    const {getNamedAccounts, deployments} = hre;
     // Deploy function and log function
     const {deploy, log} = deployments;
+    // From the hardhat.config
+    const {deployer} = await getNamedAccounts();
+    log("Deploying Governance token...");
 
+    const governanceToken = await deploy("GovernanceToken", {
+        from: deployer,
+        args: [],
+        log: true,
+    });
+    log('Deployed!');
+
+    await delegate(governanceToken.address, deployer);
+    log("delegated!");
+};
+
+// Delegate the vote
+const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
+    const governanceToken = await ethers.getContractAt("GovernanceToken", governanceTokenAddress);
+    const tx = await governanceToken.delegate(delegatedAccount);
+    await tx.wait(1);
+    console.log(`Checkpoints: ${await governanceToken.numCheckpoints(delegatedAccount)}`);
 }
 
 export default deployGovernanceToken;
